eb279c35e7f652a2f0acc4bf00712f91ef9a44ef
ValeriCodeU <valeri.yanev@icloud.com>
2025-12-18 12:34:32 +0200

    feat: add React Router routes and navigation links

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,23 +1,24 @@
+import { Link } from "react-router";
+
 export default function Header() {
-    return(
-        <section>
-            <header>
-                {/* <!-- Navigation --> */}
-                <nav>
-                    <a className="home" href="#"> <img src="./images/logo.png" alt="logo" /> </a>
-                    <a href="#">Catalog</a>
-                    {/* <!-- Logged-in users --> */}
-                    <div id="user">
-                        <a href="#">Add Game</a>
-                        <a href="#">Logout</a>
-                    </div>
-                    {/* <!-- Guest users --> */}
-                    <div id="guest">
-                        <a href="#">Login</a>
-                        <a href="#">Register</a>
-                    </div>
-                </nav>
-            </header>
-        </section>
+    return (
+
+        <header>
+            {/* <!-- Navigation --> */}
+            <nav>
+                <Link className="home" to="/"> <img src="./images/logo.png" alt="logo" /> </Link>
+                <Link to="/games">Catalog</Link>
+                {/* <!-- Logged-in users --> */}
+                <div id="user">
+                    <Link to="#">Add Game</Link>
+                    <Link to="#">Logout</Link>
+                </div>
+                {/* <!-- Guest users --> */}
+                <div id="guest"> 
+                    <Link to="#">Login</Link>
+                    <Link to="#">Register</Link>
+                </div>
+            </nav>
+        </header>
     );
 }
