683d1cebe6110aa5c1b39eaea6d0a5a2129a53a8
ValeriCodeU <valeri.yanev@icloud.com>
2025-12-19 13:58:56 +0200

    feat: create game

--- client/src/components/game-create/GameCreate.jsx
+++ client/src/components/game-create/GameCreate.jsx
@@ -1,34 +1,81 @@
+import { useNavigate } from "react-router";
+import Swal from "sweetalert2";
+
 export default function GameCreate() {
+
+    const navigate = useNavigate();
+
+    const createGameHander = async (e) => {
+        e.preventDefault();
+
+        const formdData = new FormData(e.target);
+
+        const data = Object.fromEntries(formdData);
+
+        data.players = Number(data.players);
+        data._createdOn = Date.now();
+
+        try {
+
+            const response = await fetch('http://localhost:3030/jsonstore/games', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+
+            if (!response.ok) {
+                throw new Error('Failed to create game!');
+            }
+
+            const result = await response.json();
+
+            navigate('/games');
+
+            await Swal.fire({
+                title: "✅ Success!",
+                text: `${result.title} has been created successfully!`,
+            });
+
+        } catch (error) {
+            Swal.fire({
+                title: "❌ Error!",
+                text: error.message,
+            });
+        }
+    }
+
     return (
-        <section id="add-page">
-            <htmlForm id="add-new-game">
+        <section id="add-page" onSubmit={createGameHander}>
+            <form id="add-new-game">
                 <div className="container">
 
                     <h1>Add New Game</h1>
 
                     <div className="htmlForm-group-half">
                         <label htmlFor="gameName">Game Name:</label>
-                        <input type="text" id="gameName" name="gameName" placeholder="Enter game title..."/>
+                        <input type="text" id="gameName" name="title" placeholder="Enter game title..." />
                     </div>
 
                     <div className="htmlForm-group-half">
                         <label htmlFor="genre">Genre:</label>
-                        <input type="text" id="genre" name="genre" placeholder="Enter game genre..."/>
+                        <input type="text" id="genre" name="genre" placeholder="Enter game genre..." />
                     </div>
 
                     <div className="htmlForm-group-half">
                         <label htmlFor="activePlayers">Active Players:</label>
-                        <input type="number" id="activePlayers" name="activePlayers" min="0" placeholder="0"/>
+                        <input type="number" id="activePlayers" name="players" min="0" placeholder="0" />
                     </div>
 
                     <div className="htmlForm-group-half">
                         <label htmlFor="releaseDate">Release Date:</label>
-                        <input type="date" id="releaseDate" name="releaseDate"/>
+                        <input type="date" id="releaseDate" name="date" />
                     </div>
 
                     <div className="htmlForm-group-full">
                         <label htmlFor="imageUrl">Image URL:</label>
-                        <input type="text" id="imageUrl" name="imageUrl" placeholder="Enter image URL..."/>
+                        <input type="text" id="imageUrl" name="imageUrl" placeholder="Enter image URL..." />
                     </div>
 
                     <div className="htmlForm-group-full">
@@ -38,7 +85,7 @@ export default function GameCreate() {
 
                     <input className="btn submit" type="submit" value="ADD GAME" />
                 </div>
-            </htmlForm>
+            </form>
         </section>
 
     );
