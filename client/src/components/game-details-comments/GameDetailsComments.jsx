cfdbd1c5080b04bda7f4024f2140dffd58ed096e
ValeriCodeU <valeri.yanev@icloud.com>
2025-12-31 20:14:32 +0200

    feat: display comment author

--- client/src/components/game-details-comments/GameDetailsComments.jsx
+++ client/src/components/game-details-comments/GameDetailsComments.jsx
@@ -6,7 +6,7 @@ export default function GameDetailsComments({
             <h2>Comments:</h2>
             <ul>
                 {comments.map(c => (<li className="comment" key={c._id}>
-                    <p>{c.commentText}</p>
+                    <p>{c.author}: {c.commentText}</p>
                 </li>))}
             </ul>
             {/* <!-- Display paragraph: If there are no games in the database --> */}
