3576f54524196f399972624fb9893f33ff661a30
ValeriCodeU <valeri.yanev@icloud.com>
2026-01-01 17:43:21 +0200

    feat: add image upload preview with toggle option

--- client/src/components/game-create/GameCreate.jsx
+++ client/src/components/game-create/GameCreate.jsx
@@ -1,18 +1,30 @@
 import { useNavigate } from "react-router";
 import Swal from "sweetalert2";
 import request from "../../utils/request";
+import { useEffect, useState } from "react";
 
 export default function GameCreate() {
 
+    const [imageUpload, setImageUpload] = useState(false);
+    const [imagePreview, setImagePreview] = useState('');
     const navigate = useNavigate();
+
+    useEffect(() => {
+        return () => {
+            if (imagePreview) {
+                URL.revokeObjectURL(imagePreview);                     
+            }
+        };
+    }, [imageUpload,imagePreview]);
 
     const createGameHander = async (e) => {
         e.preventDefault();
 
         const formdData = new FormData(e.target);
 
-        const data = Object.fromEntries(formdData);
+        const {image, ...data} = Object.fromEntries(formdData);
 
+        data.imageUrl = image;
         data.players = Number(data.players);
         data._createdOn = Date.now();
 
@@ -42,13 +54,36 @@ export default function GameCreate() {
                 text: `${result.title} has been created successfully!`,
             });
 
-        } catch (err) {         
+        } catch (err) {
 
             await Swal.fire({
                 title: "❌ Error!",
                 text: err.message,
             });
         }
+    }
+
+    const imageChangeHandler = (e) => {
+        const image = e.target.files[0];
+
+        const imageUrl = URL.createObjectURL(image);
+        // setImagePreview(imageUrl);
+        // чистим стария preview ако има
+        setImagePreview(prev => {
+            if (prev) URL.revokeObjectURL(prev);
+            return imageUrl;
+        });
+
+        console.log(imageUrl);
+    }
+
+    const imageUploadClickHandler = () => {
+        setImageUpload(state => !state);
+        // setImagePreview('');
+        setImagePreview(prev => {
+            if (prev) URL.revokeObjectURL(prev);
+            return '';
+        });
     }
 
     return (
@@ -79,8 +114,16 @@ export default function GameCreate() {
                     </div>
 
                     <div className="htmlForm-group-full">
-                        <label htmlFor="imageUrl">Image URL:</label>
-                        <input type="text" id="imageUrl" name="imageUrl" placeholder="Enter image URL..." />
+                        <label htmlFor="image">{imageUpload ? 'Image Upload:' : 'Image Url:'}</label>
+                        <button className="details-button" onClick={imageUploadClickHandler} type="button">Image Type</button>
+                        {imageUpload ?
+                            <input type="file" className="file-upload-btn" id="image" name="image" onChange={imageChangeHandler} placeholder="Upload image..." />
+                            :
+                            <input type="text" id="image" name="image" placeholder="Enter image URL..." />}
+
+                        {imagePreview && (
+                            <img src={imagePreview} alt="preview image" />
+                        )}
                     </div>
 
                     <div className="htmlForm-group-full">
